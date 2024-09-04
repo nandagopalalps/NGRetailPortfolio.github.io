b6966d8bd650edd634938b85b000334fbf40a5a0
nandagopalalps <nandagopal.r@gmail.com>
2024-09-04 11:35:16 +1000

    Retail Portfolio - adding more scenarios and tests

--- tests/retailportfolio.spec.ts
+++ tests/retailportfolio.spec.ts
@@ -2,60 +2,42 @@ import { test, expect } from "@playwright/test";
 
 test("Retail Portfolio - Checkout Item", async ({ page, request }) => {
   await page.goto("https://www.demoblaze.com/");
-  //   await page.getByRole('link', { name: 'Sign up' }).click();
-  //   await page.getByLabel('Username:').click();
-  //   await page.getByLabel('Username:').fill('nandan');
-  //   await page.getByLabel('Username:').press('Tab');
-  //   await page.getByLabel('Password:').fill('nandan');
-  //   page.once('dialog', dialog => {
-  //     console.log(`Dialog message: ${dialog.message()}`);
-  //     dialog.dismiss().catch(() => {});
-  //   });
-  //   await page.getByRole('button', { name: 'Sign up' }).click();
-  //   await page.getByLabel('Username:').click();
-  //   await page.getByLabel('Username:').click({
-  //     clickCount: 3
-  //   });
-  //   await page.getByLabel('Username:').fill('ngalps');
-  //   await page.getByLabel('Username:').press('Tab');
-  //   await page.getByLabel('Password:').fill('ngalps');
-  //   page.once('dialog', dialog => {
-  //     console.log(`Dialog message: ${dialog.message()}`);
-  //     dialog.dismiss().catch(() => {});
-  //   });
-  //   await page.getByRole('button', { name: 'Sign up' }).click();
   await page.getByRole("link", { name: "Log in" }).click();
-
   await page.locator("#loginusername").fill("ngalps");
-
   await page.locator("#loginpassword").fill("ngalps");
   await page.getByRole("button", { name: "Log in" }).click();
-  const itemname = "Samsung galaxy s6";
+  const itemresponse = await request.get("https://api.demoblaze.com/entries");
+  console.log(`Item Response:${itemresponse.status()}`);
+  console.log(await itemresponse.headersArray());
+  const items = await itemresponse.json();
+  const firstitem = items["Items"][0];
+  console.log(firstitem["title"]);
+  const itemname = firstitem["title"];
+  const itemprice = firstitem["price"];
   await page.getByRole("link", { name: itemname }).click();
   page.once("dialog", (dialog) => {
     console.log(`Dialog message: ${dialog.message()}`);
     dialog.dismiss().catch(() => {});
   });
   await expect(page.getByRole("heading", { name: itemname })).toBeVisible();
+  const actualprice = page.getByRole("heading", {
+    name: `$${itemprice} *includes tax`,
+  });
+  await expect(actualprice).toBeVisible();
   await page.getByRole("link", { name: "Add to cart" }).click();
   await page.getByRole("link", { name: "Cart", exact: true }).click();
   await expect(page.getByRole("cell", { name: itemname })).toBeVisible();
-
+  await expect(page.getByRole("cell", { name: itemprice })).toBeVisible();
   await page.getByRole("button", { name: "Place Order" }).click();
-
+  await expect(page.getByText("Total:")).toContainText(`${itemprice}`);
   await page.getByLabel("Total:").fill("NG");
-
   await page.getByLabel("Country:").fill("AUSTRALIA");
-
   await page.getByLabel("City:").fill("GEELONG");
-
   await page.getByLabel("Credit card:").fill("5555666677778888");
-
   await page.getByLabel("Month:").fill("09");
-
   await page.getByLabel("Year:").fill("2026");
   await page.getByRole("button", { name: "Purchase" }).click();
-  //await page.waitForTimeout(1000);
+
   const delresponse = await request.post(
     "https://api.demoblaze.com/deletecart",
     {
@@ -63,7 +45,8 @@ test("Retail Portfolio - Checkout Item", async ({ page, request }) => {
     }
   );
   await expect(delresponse).toBeOK();
+  await expect(page.getByText(`${itemprice} USD`)).toBeVisible();
+  await page.waitForTimeout(500);
   await page.getByRole("button", { name: "OK" }).click();
-
   await page.getByRole("link", { name: "Log out" }).click();
 });
